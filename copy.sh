9299e32fdd483d443f72480432b73923eb16e12f
vadim.ruban <vadim.ruban@carusto.com>
2016-10-28 16:16:36 +0300

    Added Calls APIs and refactor existing ones

--- copy.sh
+++ copy.sh
@@ -2,6 +2,8 @@
 
 rm -f ~/Projects/carusto-testkit/src/main/java/com/carusto/testkit/api/*
 rm -f ~/Projects/carusto-testkit/src/main/java/com/carusto/testkit/models/*
+rm -rf ~/Projects/carusto-testkit/src/main/java/io/swagger/client/*
 
 cp ./build/java/src/main/java/com/carusto/testkit/api/* ~/Projects/carusto-testkit/src/main/java/com/carusto/testkit/api/
 cp ./build/java/src/main/java/com/carusto/testkit/models/* ~/Projects/carusto-testkit/src/main/java/com/carusto/testkit/models/
+cp -r ./build/java/src/main/java/io/swagger/client/* ~/Projects/carusto-testkit/src/main/java/io/swagger/client/
